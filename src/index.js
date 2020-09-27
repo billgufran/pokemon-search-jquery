831f2996738a7c2f208d66bafe7e76c96b1a1884
billgufran <billgufran@gmail.com>
2020-09-27 18:08:04 +0800

    Add card flipping hint

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ import $ from "jquery";
 import "./components/PokemonCard.js";
 import "./style/style.css";
 
-const name = () => $('#search-input').val().toLowerCase()
+$(function() {
+   setTimeout(function() {
+      $(".card-flip").addClass("flipped");
+      $('.back').addClass('blink')
+   }, 4000)
+   setTimeout(function() {
+      $('.front').addClass('blink')
+      $(".card-flip").removeClass("flipped")
+   }, 4800)
+   setTimeout(function() {
+      $('.front').removeClass('blink')
+      $('.back').removeClass('blink')
+   }, 6000)
+})
+
+const name = () => $("#search-input").val().toLowerCase();
 
 $("#search-form").on("submit", event => {
-   event.preventDefault();
-   $('pokemon-card').attr('name', name);
-   $('.card-flip').removeClass('flipped');
+	event.preventDefault();
+	$("pokemon-card").attr("name", name);
+	$(".card-flip").removeClass("flipped");
 });
 
-$('.card-flip').on("click", function(){
-   $(this).toggleClass('flipped');
- });
+$(".card-flip").on("click", event => {
+   $(event.currentTarget).toggleClass("flipped");
+});
